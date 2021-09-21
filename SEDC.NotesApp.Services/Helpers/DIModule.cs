5d4fe76fbdfdb54b49229540c0c3f16cbb7cef1c
Igor Tarchugovski <Igor.t93@outlook.com>
2021-09-21 18:42:04 +0200

    Add project files.

---
+++ SEDC.NotesApp.Services/Helpers/DIModule.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEDC.NotesApp.Models;
+using SEDC.NotesApp.Models.DbModels;
+using SEDC.NotesApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.NotesApp.Services.Helpers
+{
+    public static class DIModule
+    {
+        public static IServiceCollection RegisterModule(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<NotesAppDbContext>(
+                opt => opt.UseSqlServer(connectionString)
+            );
+
+            // register repositories
+            services.AddTransient<IRepository<Note>, NoteRepository>();
+
+            return services;
+        }
+    }
+}
